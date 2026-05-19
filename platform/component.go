9f8d7ff8ffd2ba0517540225461beda71704dcfb
Benjamin Eich <benjamin-eich@web.de>
2026-05-19 10:38:24 +0200

    Add `CertificateComponent` struct and related types for XML handling

--- platform/component.go
+++ platform/component.go
@@ -1,5 +1,7 @@
 package platform
 
+import "encoding/xml"
+
 type Component struct {
 	ComponentId                string         `json:"componentId"`
 	Version                    int            `json:"version"`
@@ -22,4 +24,60 @@ type Component struct {
 	EncryptedValues            any            `json:"encryptedValues"`
 	Description                string         `json:"description"`
 	Object                     map[string]any `json:"object"`
+}
+
+type CertificateComponent struct {
+	XMLName xml.Name `xml:"Component"`
+
+	FolderFullPath string `xml:"folderFullPath,attr,omitempty"`
+	ComponentId    string `xml:"componentId,attr"`
+	Version        int    `xml:"version,attr"`
+	Name           string `xml:"name,attr"`
+	Type           string `xml:"type,attr"`
+	SubType        string `xml:"subType,attr,omitempty"`
+	CreatedDate    string `xml:"createdDate,attr"`
+	CreatedBy      string `xml:"createdBy,attr"`
+	ModifiedDate   string `xml:"modifiedDate,attr"`
+	ModifiedBy     string `xml:"modifiedBy,attr"`
+	Deleted        bool   `xml:"deleted,attr"`
+	CurrentVersion bool   `xml:"currentVersion,attr"`
+	FolderName     string `xml:"folderName,attr"`
+	FolderId       string `xml:"folderId,attr"`
+	BranchName     string `xml:"branchName,attr"`
+	BranchId       string `xml:"branchId,attr"`
+
+	Description string `xml:"description"`
+
+	Object CertificateObject `xml:"object"`
+}
+
+type CertificateObject struct {
+	Certificate Certificate `xml:"CertificateModel"`
+}
+
+type Certificate struct {
+	MD5Fingerprint     string `xml:"MD5Fingerprint,attr"`
+	SHA1Fingerprint    string `xml:"SHA1Fingerprint,attr"`
+	SerialNumber       string `xml:"serialNumber,attr"`
+	SignatureAlgorithm string `xml:"signatureAlgorithm,attr"`
+	Version            string `xml:"version,attr"`
+
+	Type            string              `xml:"Type"`
+	IssuedTo        CertificateIdentity `xml:"IssuedTo"`
+	Issuer          CertificateIdentity `xml:"Issuer"`
+	Validity        Validity            `xml:"Validity"`
+	CertificateData string              `xml:"CertificateData"`
+}
+
+type CertificateIdentity struct {
+	CommonName             string `xml:"commonName,attr"`
+	Country                string `xml:"country,attr"`
+	FullName               string `xml:"fullName,attr"`
+	Organization           string `xml:"organization,attr,omitempty"`
+	OrganizationalUnitName string `xml:"organizationalUnit,attr,omitempty"`
+}
+
+type Validity struct {
+	ExpireDate string `xml:"expireDate,attr"`
+	IssueDate  string `xml:"issueDate,attr"`
 }
